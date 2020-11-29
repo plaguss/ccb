0b68644bdb4332b46d4d5bb912ea2f7c5d26f251
Agustin <agustin.piqueres@gmail.com>
2020-11-29 19:56:26 +0100

    initial commit

---
+++ ccb/activities.py
@@ -0,0 +1,357 @@
+"""
+Classes offered by the gym.
+When defined, the methods let you sign up for one in case there
+is a vacancy.
+"""
+
+import typing
+import selenium.webdriver.remote.webelement as we
+import warnings
+import logging
+
+
+# Set default message from config to be info, and prettier format:
+logging.basicConfig(format='%(asctime)s --> %(levelname)s: %(message)s', level=logging.INFO)
+
+
+class Reservation:
+    """
+    Processes the 'Reservas' elements.
+    Number of places in a given Activity.
+
+    Parameters
+    ----------
+    reserva : str
+        Element representing the number of places in a given activity.
+        Its structure is (<PLACES_LEFT>/<TOTAL_PLACES>)
+
+    Methods
+    -------
+    is_free
+
+    Attributes
+    ----------
+    places
+    total
+
+    Examples
+    --------
+    >>> reserva = '(13/15)'
+    >>> reserv = Reservation(reserva)
+    >>> reserv.is_free()
+    True
+    """
+    def __init__(self, reserva: str) -> None:
+        self._reserva = reserva
+        places, total = reserva[1:-1].split('/')
+        self.places = places
+        self.total = total
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, str(self))
+
+    def __str__(self):
+        return self._reserva
+
+    @property
+    def places(self) -> int:
+        """Contains the number of places on a given activity. """
+        return self._places
+
+    @places.setter
+    def places(self, plc: str):
+        self._places = int(plc)
+
+    @property
+    def total(self) -> int:
+        """Contains the total number of places on a given activity. """
+        return self._total
+
+    @total.setter
+    def total(self, tot: str):
+        self._total = int(tot)
+
+    def is_free(self) -> bool:
+        """Returns True if places < total, false otherwise.
+
+        Returns
+        -------
+        free : bool
+            Checks whether is place in a given activity.
+        """
+        return self.places < self.total
+
+
+class Hour:
+    """Simple Hour format to represent a class.
+    Allows different operations between them to check if
+    a reservation deserves taking a place or not.
+
+    Parameters
+    ----------
+    h : str
+        String representing an hour of the format hh:mm, i.e. 20:30.
+
+    Examples
+    --------
+    >>> h = '11:30'
+    >>> hour = Hour(h)
+    >>> h2 = '13:30'
+    >>> hour2 = Hour(h2)
+    >>> hour == hour2
+    False
+    >>> hour <= hour2
+    True
+    >>> hour >= hour2
+    """
+    def __init__(self, h: str) -> None:
+        self._h = h
+        hour, minutes = h.split(':')
+        self.hour = hour
+        self.minutes = minutes
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, str(self))
+
+    def __str__(self):
+        return self._h
+
+    @property
+    def hour(self) -> int:
+        return self._hour
+
+    @hour.setter
+    def hour(self, hr: str) -> None:
+        self._hour = int(hr)
+
+    @property
+    def minutes(self) -> int:
+        return self._minutes
+
+    @minutes.setter
+    def minutes(self, mins: str) -> None:
+        self._minutes = int(mins)
+
+    def __eq__(self, other: 'Hour') -> bool:
+        if not isinstance(other, Hour):
+            raise ValueError('{} must be an Hour instance.'.format(other))
+
+        if self.hour == other.hour:
+            return self.minutes == other.minutes
+        else:
+            return False
+
+    def __le__(self, other: 'Hour') -> bool:
+        if not isinstance(other, Hour):
+            raise ValueError('{} must be an Hour instance.'.format(other))
+
+        if self.hour <= other.hour:
+            return True
+        else:
+            if self.hour == other.hour:
+                return self.minutes <= other.minutes
+
+    def __lt__(self, other: 'Hour') -> bool:
+        if not isinstance(other, Hour):
+            raise ValueError('{} must be an Hour instance.'.format(other))
+
+        if self.hour < other.hour:
+            return True
+        else:
+            if self.hour == other.hour:
+                return self.minutes < other.minutes
+
+    def __ge__(self, other: 'Hour') -> bool:
+        return not self <= other
+
+    def __gt__(self, other: 'Hour') -> bool:
+        return not self < other
+
+
+class Schedule:
+    """Contains the class to deal with the hours a given class takes place.
+
+    Parameters
+    ----------
+    sch : str
+        Represents the hour of the class as a string. i.e. '11:00 - 13:00'
+
+    Examples
+    --------
+    >>> sch = '11:00 - 13:00'
+    >>> schedule = Schedule(sch)
+    >>> schedule.start
+    Hour(11:00)
+
+    >>> hour = Hour('11:30')
+    >>> hour2 = Hour('16:00')
+    >>> hour in schedule
+    True
+    >>> hour2 in schedule
+    False
+    """
+    def __init__(self, sch: str) -> None:
+        self._sch = sch
+        start, end = sch.split(' - ')
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, str(self))
+
+    def __str__(self):
+        return self._sch
+
+    def __contains__(self, item: Hour) -> bool:
+        return self.start < item < self.end
+
+    @property
+    def start(self) -> Hour:
+        """Start hour. """
+        return self._start
+
+    @start.setter
+    def start(self, strt: str) -> None:
+        self._start = Hour(strt)
+
+    @property
+    def end(self) -> Hour:
+        """End hour. """
+        return self._end
+
+    @end.setter
+    def end(self, end_: str) -> None:
+        self._end = Hour(end_)
+
+
+class ButtonIcon:
+    """
+    Reference for plus/minus icons of a button.
+    A PLUS button appears when there is a vacancy, a
+    MINUS button appears when you are already signed
+    for a class, checking it would leave the reservation.
+    """
+    PLUS = 'PLUS'
+    MINUS = 'MINUS'
+
+
+class Button:
+    """
+    Element of selenium representing the button to be pressed to book a class.
+
+    TODO:
+        determinar si un botón es glyphicon-minus para NO clickar.
+    """
+    def __init__(self, element: typing.Union[we.WebElement, str], icon: typing.Union[str, None] = None) -> None:
+        if isinstance(element, str):
+            self._enabled = False
+            self._icon = icon
+        else:  # Properly defined button.
+            self.element = element
+            self._enabled = True
+            self.icon = icon
+
+    def __repr__(self):
+        if self.icon is not None:
+            icon = self.icon
+        else:
+            icon = ''
+        return "{}({} - {})".format(self.__class__.__name__, self.is_enabled(), icon)
+
+    def is_enabled(self) -> bool:
+        """Check whether a button can be checked or not. """
+        return self._enabled
+
+    @property
+    def element(self) -> typing.Union[we.WebElement, None]:
+        """WebElement containing the element to be clicked. Or None if could not be found. """
+        return self._element
+
+    @element.setter
+    def element(self, el: we.WebElement) -> None:
+        self._element = el
+
+    @property
+    def icon(self) -> typing.Union[str, None]:
+        """WebElement containing the element to be clicked. Or None if could not be found. """
+        return self._icon
+
+    @icon.setter
+    def icon(self, ico: str) -> None:
+        if 'plus' in ico:
+            ico_ = ButtonIcon.PLUS
+        else:
+            ico_ = ButtonIcon.MINUS
+        self._icon = ico_
+
+    def click(self) -> None:
+        """Clicks the button, then registering to a class. """
+        if self.is_enabled():
+            self.element.click()
+        else:
+            warnings.warn('The Button cannot be clicked.')
+
+
+class Activities:
+    CROSSFIT = 'Crossfit'
+    OPEN_BOX = 'Open Box'
+    WEIGHTLIFTING = 'Halterofília'
+    CALISTHENICS = 'Calisteni'
+
+
+class Activity:
+    """
+    TODO: Definir completamente activity.
+    """
+    def __init__(
+            self,
+            schedule: typing.Union[Schedule, None] = None,
+            reservation: typing.Union[Reservation, None] = None,
+            button: typing.Union[Button, None] = None
+    ) -> None:
+        self.schedule = schedule
+        self.reservation = reservation
+        self.button = button
+
+    def __repr__(self):
+        return '{}({}, {})'.format(self.__class__.__name__, self.schedule, self.reservation)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def book(self) -> None:
+        """Returns True if there was space and the class could be registered,
+        False otherwise.
+        """
+        # Check for space
+        if self.reservation.is_free():
+            self.button.click()
+            logging.info('Class registered: {}'.format(self))
+        else:
+            logging.info('No space at the moment')
+
+
+class OpenBox(Activity):
+    """Activity subclass for an OpenBox activity. """
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+
+class Crossfit(Activity):
+    """Activity subclass for a Crossfit class. """
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+
+class Calisthenics(Activity):
+    """Activity subclass for a Calisthenics class. """
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+
+class Weightlifting(Activity):
+    """Activity subclass for a Weightlifting class. """
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+
---
+++ ccb/ccb.py
@@ -0,0 +1,424 @@
+"""
+Module automating crossfit registration.
+
+chromedriver:
+https://chromedriver.storage.googleapis.com/index.html?path=86.0.4240.22/
+
+Esperar a que un elemento esté presente:
+https://stackoverflow.com/questions/26566799/wait-until-page-is-loaded-with-selenium-webdriver-for-python
+"""
+
+
+import logging
+import time
+import datetime as dt
+import typing
+import os
+import sys
+import warnings
+import json
+
+import selenium.webdriver as wd
+from selenium.webdriver.common.by import By
+import selenium.webdriver.remote.webelement as we
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
+
+# Set default message from config to be info, and prettier format:
+logging.basicConfig(format='%(asctime)s --> %(levelname)s: %(message)s', level=logging.INFO)
+
+up = os.path.dirname
+here = up(up(os.path.abspath(__file__)))
+if here not in sys.path:
+    sys.path.append(here)
+
+import crack_ccb.activities as act
+
+
+WAIT_FOR_CLOSE = 5  # Wait 5 seconds before closing the page.
+
+
+# 1) acceso clientes:
+URL = r'https://www.crossfitcostablanca.es/acceso-a-clientes/'
+# 2) login:
+LOGIN_URL = r'https://www.crossfitcostablanca.es/login.php'
+
+
+CLASS_MAP = {
+    'Open Box': act.Activities.OPEN_BOX,
+    'Crossfit': act.Activities.CROSSFIT,
+    'Calisthenics': act.Activities.CALISTHENICS,
+    'Weightlifting': act.Activities.WEIGHTLIFTING
+}
+
+
+class ClassError(KeyError):
+    def __init__(self, class_, message="Class not defined."):
+        self.class_ = class_
+        self.message = message + " Must be one of: {}.".format(list(CLASS_MAP.keys()))
+        super().__init__(self.message)
+
+
+class NoHoursError(ValueError):
+    def __init__(self, message="No hours inserted or incorrect format."):
+        # self.hours = hours
+        self.message = message
+        super().__init__(self.message)
+
+
+class JsonConfig:
+    """
+    Sample config file:
+    {
+    "Username": "agustin.piqueres@gmail.com",
+    "Password": "2293",
+    "wanted_classes": ["Open Box", "Crossfit"],
+    "wanted_hours": ["11:00"],
+    "wanted_days": ["22/11/2020"]
+    }
+    Must contain 5 elements:
+        - Username: string with the username. In general it should be
+        your email account.
+        - Password: string with the password.
+        - wanted_classes: list with the classes you would like to attend.
+        Must be one of Open Box, Crossfit, Calisthenics or Weightlifting.
+        - wanted_hours: list with the hours you are interested to assist.
+        Must be in format hh:mm.
+        - wanted_days : list of days you are interested to check for the
+        classes. Must be in format dd/mm/yyyy
+
+    Parameters
+    ----------
+    path : str
+        Full path to json config file.
+
+    Examples
+    --------
+    >>> config = JsonConfig(CONFIG)
+    >>> config.wanted_classes()
+    ['Open Box', 'Crossfit']
+    >>> config.wanted_days()
+    [datetime.date(2020, 11, 22)]
+    >>> config.wanted_hours()
+    [Hour(11:00)]
+    """
+    def __init__(self, path: str) -> None:
+        self._path = path
+        self._data = None
+        self._read_file()
+        self._classes = []
+        self._hours = []
+        self._days = []
+
+    def _read_file(self) -> None:
+        """Parses the json config file and stores the info in _data attribute. """
+        with open(self._path) as f:
+            self._data = json.load(f)
+
+    def _get_classes(self) -> None:
+        """
+        Parse the class string to one defined in Activities, which are present in
+        the page.
+        """
+        for wanted_class in self._data['wanted_classes']:
+            if wanted_class not in CLASS_MAP.keys():
+                raise ClassError(wanted_class)
+            activity = CLASS_MAP[wanted_class]
+            self._classes.append(activity)
+
+    def submit_info(self) -> typing.Tuple[str, str]:
+        """Returns a tuple with the username and password. """
+        return self._data['Username'], self._data['Password']
+
+    def wanted_classes(self) -> typing.List[act.Activities]:
+        """Returns a list with classes defined as act.Activities. """
+        if len(self._classes) == 0:
+            self._get_classes()
+        return self._classes
+
+    def wanted_hours(self) -> typing.List[act.Hour]:
+        """Hour objects to check for a place. """
+        for h in self._data['wanted_hours']:
+            self._hours.append(act.Hour(h))
+
+        if len(self._hours) == 0:
+            raise NoHoursError()
+
+        return self._hours
+
+    def wanted_days(self) -> typing.List[dt.date]:
+        """Days to check for a class, as a list of dt.date objects. """
+        for day in self._data['wanted_days']:
+            self._days.append(self._parse_day(day))
+        return self._days
+
+    @staticmethod
+    def _parse_day(day: str) -> dt.date:
+        """Creates a dt.date object. """
+        try:
+            d, m, y = day.split('/')
+            return dt.date(int(y), int(m), int(d))
+        except ValueError:
+            raise ValueError("Bad day format in 'wanted_days'. ")
+
+
+class CCB:
+    """Interact with Crossfit Costa Blanca web page.
+
+    Parameters
+    ----------
+    webdriver_ : webdriver
+        webdriver to use from selenium. Defaults to Chrome.
+        Only one tested.
+
+    Methods
+    -------
+    login_page
+    set_username
+    set_password
+    submit
+
+    """
+    def __init__(self, webdriver_: str = 'chrome') -> None:
+        self.driver = webdriver_
+
+    @property
+    def driver(self) -> wd.Chrome:
+        """WebDriver object from selenium. Interactive web page object."""
+        return self._driver
+
+    @driver.setter
+    def driver(self, drv: str) -> None:
+        if drv.lower() == 'chrome':
+            # Get the path to the chromedriver.exe of the project.
+            driver_path = os.path.join(here, 'crack_ccb', 'chromedriver.exe')
+            logging.info(driver_path)
+
+            # Set headless mode to avoid opening the browser
+            options = wd.ChromeOptions()
+            options.add_argument('headless')
+
+            # self._driver = wd.Chrome(driver_path, options=options)
+            self._driver = wd.Chrome(driver_path)
+            self._driver.maximize_window()
+        else:
+            raise NotImplementedError(
+                "Only tested for 'chrome', implement yourself other driver."
+            )
+
+    def login_page(self) -> None:
+        """Enters to the login page. """
+        self.driver.get(LOGIN_URL)
+        logging.info('CCB accessed.')
+
+    def set_username(self, user: str) -> None:
+        """Sends the username to the page.
+
+        Parameters
+        ----------
+        user : str
+            Username
+        """
+        username = self.driver.find_element(By.NAME, 'Email')
+        username.send_keys(user)
+        logging.info('Username registered.')
+
+    def set_password(self, passwd: str) -> None:
+        """Sends the password to the page.
+
+        Parameters
+        ----------
+        passwd : str
+            Password
+        """
+        password = self.driver.find_element(By.NAME, 'passwd')
+        password.send_keys(passwd)
+        logging.info('Password registered.')
+
+    def submit(self, username: str, password: str) -> None:
+        """
+        Sends Username and Password and press submit button.
+
+        Parameters
+        ----------
+        username : str
+            Username to be passed to set_username.
+        password : str
+            Password to be passed to set_username.
+
+        Returns
+        -------
+        None
+        """
+        self.set_username(username)
+        self.set_password(password)
+        self.driver.find_element(By.CSS_SELECTOR, '.btn-primary').click()
+
+    def get_day(self, day: dt.date) -> None:
+        """Get the button of a given day, inserted as a datetime.date object.
+
+        Extracts the day, passes it to str to find it, and pushes its button.
+
+        Parameters
+        ----------
+        day : dt.date
+            Day wanted to find.
+
+        Examples
+        --------
+        >>> day = dt.date.today()
+        >>> ccb.get_day(day)
+        """
+        strday = str(day.day)
+        # Go to the end of the page.
+        # This is done to be sure the last days in the calendar are
+        # found in any type of screen.
+        self._driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        day_button = self.driver.find_element(By.LINK_TEXT, strday)
+        logging.info(day_button.get_attribute('href'))
+        day_button.click()
+        logging.info('Found the day!!')
+
+    def get_activities(self) -> typing.List[act.Activity]:
+        """Loop over elements of the table. """
+        # self.driver.find_element(By.CSS_SELECTOR, 'table-striped')
+        tables = self.driver.find_elements(By.CLASS_NAME, 'table-striped')
+        # There should be 2 tables, the first contains the days.
+        # Clicking on a day displays the second table with the same class name,
+        # containing the activities.
+        table = tables[1]
+        logging.info('table found!')
+        activities = []
+        for i, row in enumerate(table.find_elements(By.CSS_SELECTOR, 'tr')):
+            if i > 1:  # The first says the Activities of the day, the second the names of each column.
+                row_elements = []
+                cells = row.find_elements(By.TAG_NAME, 'td')
+                if len(cells) > 4:
+                    # FIXME: Define a proper way of controlling this.
+                    # In this case we are already registered in a class, don't do anything yet
+                    pass
+
+                else:
+                    for j, cell in enumerate(cells):
+                        elem = self._parse_table_elem(j, cell)
+                        row_elements.append(elem)
+                    # Store as keyword arguments the information for the activities.
+                    name = row_elements[1]
+                    arguments = {'schedule': row_elements[0], 'reservation': row_elements[2], 'button': row_elements[3]}
+                    activity = self._get_activity(arguments, name)
+                    activities.append(activity)
+                    logging.info(activity)
+
+        return activities
+
+    @staticmethod
+    def _parse_table_elem(
+            pos: int, cell: we.WebElement
+    ) -> typing.Union[act.Schedule, act.Button, act.Reservation, str]:
+        """Parses each element of the table to its corresponding object.
+
+        Parameters
+        ----------
+        pos : int
+            Element of a row in the table of activities.
+        cell : we.WebElement
+            Element obtained from Selenium with info to create a full
+            object for the activities.
+
+        Returns
+        -------
+        elem : Schedule, Button, Reservation or str
+            Element parsed and transformed to its given class to
+            simplify its interaction inside of an Activity.
+        """
+        if pos == 0:  # Horario
+            elem = act.Schedule(cell.text)
+        elif pos == 1:  # Actividad
+            elem = cell.text
+        elif pos == 2:  # Reservas
+            elem = act.Reservation(cell.text)
+        elif pos == 3:  # Reservar
+            icon = None
+            if len(cell.text) > 0:
+                elem = cell.text
+            else:
+                elem = cell.find_element(By.CSS_SELECTOR, 'a')
+                icon = cell.find_element_by_css_selector('span').get_attribute('class')
+            elem = act.Button(elem, icon=icon)
+        else:
+            raise ValueError('This element is not expected: {}'.format(cell))
+
+        return elem
+
+    @staticmethod
+    def _get_activity(arguments: typing.Dict, name: str) -> act.Activity:
+        """Get the corresponding activity by the parsed info.
+
+        Parameters
+        ----------
+        arguments : dict
+            Contains the dict with the names of the variables to instantiate
+            a given activity, and the proper object as a value.
+        name : str
+            The parsed element indicating the type of activity.
+
+        Returns
+        -------
+        activity : act.Activity
+            Activity instantiated.
+        """
+        if name == act.Activities.OPEN_BOX:
+            activity = act.OpenBox(**arguments)
+
+        elif name == act.Activities.CROSSFIT:
+            activity = act.Crossfit(**arguments)
+
+        elif name == act.Activities.CALISTHENICS:
+            activity = act.Calisthenics(**arguments)
+
+        elif name == act.Activities.WEIGHTLIFTING:
+            activity = act.Weightlifting(**arguments)
+
+        else:
+            activity = None
+            warnings.warn('Activity unregistered: {}.'.format(activity))
+
+        return activity
+
+    def close_page(self) -> None:
+        time.sleep(2)
+        self.driver.close()
+
+
+if __name__ == '__main__':
+
+    parent = os.path.dirname(os.path.abspath(__file__))
+    CONFIG_PATH = os.path.join(parent, 'config.json')
+
+    config_file = JsonConfig(CONFIG_PATH)
+    logging.info('Config file read. ')
+    ccb = CCB()
+    # Get login page of San Vicente centre..
+    ccb.login_page()
+
+    # Wait 2 seconds in case the time is needed to load the page.
+    time.sleep(2)
+
+    # Get username and password to be sent.
+    username, password = config_file.submit_info()
+    ccb.submit(username, password)
+
+    time.sleep(WAIT_FOR_CLOSE)
+    # dia = TODAY + period(days=1)
+    days = config_file.wanted_days()
+    # TODO: Loop over the list of days.
+    ccb.get_day(days[0])
+    activities = ccb.get_activities()
+    wanted_hours = config_file.wanted_hours()
+    wanted_activities = config_file.wanted_classes()
+    for activity in activities:
+        if activity in wanted_activities:
+            activity.book()
+    ccb.close_page()
